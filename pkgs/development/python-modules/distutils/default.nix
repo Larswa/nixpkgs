c5f0e2bf8d779d24b716e8b625f173b7c986134d
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-05-15 17:35:24 +0200

    fixup! python3Packages.distutils: disable failing tests

--- pkgs/development/python-modules/distutils/default.nix
+++ pkgs/development/python-modules/distutils/default.nix
@@ -68,12 +68,14 @@ buildPythonPackage {
   # jaraco-path depends ob pyobjc
   doCheck = !stdenv.hostPlatform.isDarwin;
 
-  disabledTests = lib.optionals (pythonAtLeast "3.14") [
-    #  AssertionError: assert '(?s:foo[^/]*)\\z' == '(?s:foo[^/]*)\\Z'
-    "test_glob_to_re"
+  disabledTests = [
     #  TypeError: byte_compile() got an unexpected keyword argument 'dry_run'
     "test_byte_compile"
     "test_byte_compile_optimized"
+  ]
+  ++ lib.optionals (pythonAtLeast "3.14") [
+    #  AssertionError: assert '(?s:foo[^/]*)\\z' == '(?s:foo[^/]*)\\Z'
+    "test_glob_to_re"
   ];
 
   meta = {
