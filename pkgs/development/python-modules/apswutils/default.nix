67639c5706e496b48dc2877054615438128137fe
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-05-16 02:03:42 +0200

    python3Packages.apswutils: pin setuptools 81

--- pkgs/development/python-modules/apswutils/default.nix
+++ pkgs/development/python-modules/apswutils/default.nix
@@ -4,7 +4,7 @@
   fetchFromGitHub,
 
   # build-system
-  setuptools,
+  setuptools_81,
 
   # dependencies
   apsw,
@@ -28,7 +28,7 @@ buildPythonPackage (finalAttrs: {
   };
 
   build-system = [
-    setuptools
+    setuptools_81
   ];
 
   dependencies = [
